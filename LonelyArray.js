74051434930ad8dd6960efb06a4048c348348046
Vikasguru <vikivikas41@gmail.com>
2021-12-30 17:56:09 +0530

    Week1 Challenges

---
+++ LonelyArray.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    // Write your code here
+    
+   let num = {};
+        for(let elem of a) {
+            if(!num[elem]) num[elem] = 0;
+            num[elem]++;
+        }
+        
+        for(let [elem, freq] of Object.entries(num)) {
+            if(freq === 1) return elem;
+        } 
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
